057b1cc8955b8cf641521e48c13a8b23eb763ed3
Oleksii Sydorenko <Oleksii_Sydorenko@epam.com>
2016-09-13 14:56:37 +0300

    Create new project

---
+++ Game/src/main/java/ua/epam/ArithmeticOperation.java
@@ -0,0 +1,11 @@
+package ua.epam;
+
+/**
+ * Created by Oleksii_Sydorenko on 9/13/2016.
+ */
+public class ArithmeticOperation {
+
+    public  double add(double value1, double value2){
+        return value1 + value2;
+    }
+}
---
+++ Game/src/test/java/ua/epam/ArithmeticOperationTest.java
@@ -0,0 +1,16 @@
+package ua.epam;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Oleksii_Sydorenko on 9/13/2016.
+ */
+public class ArithmeticOperationTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+}
