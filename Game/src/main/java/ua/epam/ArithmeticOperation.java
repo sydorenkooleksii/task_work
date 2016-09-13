83758047ff8fc327a8f35becfe22cf80921d64d9
Oleksii Sydorenko <Oleksii_Sydorenko@epam.com>
2016-09-13 18:03:05 +0300

    junit Before,assertEquals,ArithmeticException

--- Game/src/main/java/ua/epam/ArithmeticOperation.java
+++ Game/src/main/java/ua/epam/ArithmeticOperation.java
@@ -16,6 +16,7 @@ public class ArithmeticOperation {
         return value1 - value2;
     }
     public  double div(double value1, double value2){
+        if (value2==0) throw new ArithmeticException();
         return value1/value2;
     }
 }
--- Game/src/test/java/ua/epam/ArithmeticOperationTest.java
+++ Game/src/test/java/ua/epam/ArithmeticOperationTest.java
@@ -1,6 +1,7 @@
 package ua.epam;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,31 +10,42 @@ import static org.junit.Assert.*;
  * Created by Oleksii_Sydorenko on 9/13/2016.
  */
 public class ArithmeticOperationTest {
+    private  ArithmeticOperation a;
+    @Before
+    public void runT(){
+        a = new ArithmeticOperation();
+    }
+
     @Test
     public void testAdd() throws Exception {
-        ArithmeticOperation a = new ArithmeticOperation();
+
         double res =a.add(3,7);
-        if (res!=10) Assert.fail();
+        Assert.assertTrue(res==10);
     }
     @Test
     public void testMult() throws Exception {
-        ArithmeticOperation a = new ArithmeticOperation();
+
         double res =a.mult(3,7);
-        if (res!=21) Assert.fail();
+        Assert.assertEquals(res,21.0,1e-15);
     }
     @Test
     public void testDeduct() throws Exception {
-        ArithmeticOperation a = new ArithmeticOperation();
+
         double res =a.deduct(7,3);
-        if (res!=4) Assert.fail();
+        Assert.assertEquals(res,4.0,1e-15);
     }
     @Test
     public void testDiv() throws Exception {
-        ArithmeticOperation a = new ArithmeticOperation();
-        double res =a.div(9,3);
-        if (res!=3) Assert.fail();
-    }
 
+        double res =a.div(9,3);
+        Assert.assertEquals(res,3.0,1e-15);
+
+
+    }
+    @Test(expected=ArithmeticException.class)
+    public void testDivExaption(){
+        a.div(9.0,0.0);
+    }
 
 
 
