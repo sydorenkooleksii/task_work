54457033ca4b3823c25b88130fd02d1a67bf4aa8
Oleksii Sydorenko <Oleksii_Sydorenko@epam.com>
2016-09-15 15:30:33 +0300

    Create Task 2

---
+++ 2-Arithmetic Junit/src/main/java/ua/epam/Main.java
@@ -0,0 +1,13 @@
+package ua.epam;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+}
