aea6564af5ba41bc99f01b46f40102dd076afbd5
Oleksii Sydorenko <Oleksii_Sydorenko@epam.com>
2016-09-19 04:36:52 +0300

    Game with MVC

---
+++ Game-5/src/ua/epam/Main.java
@@ -0,0 +1,15 @@
+package ua.epam;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+
+        controller.processUser();
+
+
+    }
+}
