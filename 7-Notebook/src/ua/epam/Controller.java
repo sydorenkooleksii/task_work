e273d863b6d6c532c56eaac9681d90204606a871
Oleksii Sydorenko <Oleksii_Sydorenko@epam.com>
2016-09-22 11:00:04 +0300

    Delete coment from task 7

--- 7-Notebook/src/ua/epam/Controller.java
+++ 7-Notebook/src/ua/epam/Controller.java
@@ -22,7 +22,7 @@ public class Controller {
     public static final String REG_TEXT_AND_NUMBER_AND_SYMBOL = "[\\S\\s]{1,150}$";
     public static final String REG_HOMEPHONE = "(\\(\\d{3}\\)-\\d{3}-\\d{2}-\\d{2})?(\\d{10,12})?(^[+]\\d{3,12})?";
     public static final String REG_CELLPHONE = "(\\(\\d{3}\\)-\\d{2}-\\d{2}-\\d{3})?(\\d{10,12})?(^[+]\\d{3,12})?";
-    public static final String REG_EMAIL = "[\\w.%-]+@[\\w]+\\.[A-Za-z]{2,4}";
+    public static final String REG_EMAIL = "[\\w.%-_]+@[\\w]+\\.[A-Za-z]{2,4}";
     public static  String REG_GROUP = "(HOME)|(WORK)|(SHOP)|(SERVICE)";
     public static final String REG_DATA = "((\\d{2})/(\\d{2})/(\\d{4}))?(\\d{2}):(\\d{2}):(\\d{4})";
     public static final String REG_ADRESS = "\\d{1,5}/([A-Z][a-z]+)/([A-Z][a-z]+)/\\d{1,5}/\\d{1,5}";
@@ -38,19 +38,19 @@ public class Controller {
 
 
     public void processUser() {
-//       chekSetInputName();
-//        chekSetInputSurname();
-//        chekSetInputDaddyname();
-//        chekSetInputNickname();
-//       chekSetInputComment();
-//        chekSetInputHomephone();
-//        chekSetInputCellphone();
-//        chekSetInputCellphone2();
-//        chekSetInputEmail();
-//        chekSetInputSkype();
-//        chekSetInputGroup();
-//        chekSetInputData();
-//        chekSetChangeData();
+       chekSetInputName();
+        chekSetInputSurname();
+        chekSetInputDaddyname();
+        chekSetInputNickname();
+       chekSetInputComment();
+        chekSetInputHomephone();
+        chekSetInputCellphone();
+        chekSetInputCellphone2();
+        chekSetInputEmail();
+        chekSetInputSkype();
+        chekSetInputGroup();
+        chekSetInputData();
+        chekSetChangeData();
         chekSetAdress();
 
     }
