c6d7c0bb7b281f6c9999ef0cdd62f673fa01c98f
Oleksii Sydorenko <Oleksii_Sydorenko@epam.com>
2016-09-26 02:43:36 +0300

    Task 10

---
+++ 10-Driver/src/main/java/epam/ua/entities/Driver.java
@@ -0,0 +1,42 @@
+package epam.ua.entities;
+
+/**
+ * Created by Oleksii on 26.09.2016.
+ */
+public class Driver {
+    private Human human;
+    private DriverLicense driverLicense;
+
+    public Driver() {
+    }
+
+    public Driver(Human human, DriverLicense driverLicense) {
+        this.human = human;
+        this.driverLicense = driverLicense;
+    }
+
+    public Human getHuman() {
+        return human;
+    }
+
+    public void setHuman(Human human) {
+        this.human = human;
+    }
+
+    public DriverLicense getDriverLicense() {
+        return driverLicense;
+    }
+
+    public void setDriverLicense(DriverLicense driverLicense) {
+        this.driverLicense = driverLicense;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "human=" + human +
+                ", driverLicense=" + driverLicense +
+                '}';
+    }
+}
+
---
+++ 10-Driver/src/test/java/epam/ua/entities/DriverTest.java
@@ -0,0 +1,84 @@
+package epam.ua.entities;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Oleksii on 26.09.2016.
+ */
+public class DriverTest {
+    @Test
+    public void creatCorectRoute() throws Exception {
+        Human oleksii = new Human("Oleksii" , new Date() , Human.Gender.MALE , Human.BloodGroup.SECOND);;
+        Driver oleksiiD = new Driver(oleksii ,new DriverLicense(new Date() ,
+                DriverLicense.Category.A , new Date()));
+        GasolineCar car = new GasolineCar("Tesla", "AM5454KB", "10991099" , DriverLicense.Category.A ,
+                10 , 20 , GasolineCar.Transmission.AUTOMATIC);
+        Route route = new Route(car , oleksiiD , "Kyiv" , "Rivne");
+        System.out.println(route);
+    }
+
+
+
+
+    @Test
+    public void creatRouteWithNotDiferentCategoriesInDriverPass() throws Exception {
+        Human oleksii = new Human("Oleksii" , new Date() , Human.Gender.MALE , Human.BloodGroup.SECOND);;
+        Driver oleksiiD = new Driver(oleksii ,new DriverLicense(new Date() ,
+                DriverLicense.Category.A.C.D , new Date()));
+        GasolineCar car = new GasolineCar("Tesla", "AM5454KB", "10991099" , DriverLicense.Category.C.D ,
+                10 , 20 , GasolineCar.Transmission.AUTOMATIC);
+        Route route = new Route(car , oleksiiD , "Kyiv" , "Rivne");
+        System.out.println(route);
+    }
+
+    @Test
+    public void creatRouteWithNotDiferentCategoriesInDriverNotPass() throws Exception {
+        Human oleksii = new Human("Oleksii" , new Date() , Human.Gender.MALE , Human.BloodGroup.SECOND);;
+        Driver oleksiiD = new Driver(oleksii ,new DriverLicense(new Date() ,
+                DriverLicense.Category.A.C.D , new Date()));
+        GasolineCar car = new GasolineCar("Tesla", "AM5454KB", "10991099" , DriverLicense.Category.C ,
+                10 , 20 , GasolineCar.Transmission.AUTOMATIC);
+        Route route = new Route(car , oleksiiD , "Kyiv" , "Rivne");
+        System.out.println(route);
+    }
+
+    @Test
+    public void creatRouteWithSpoiledDataLicense() throws Exception {
+        Human oleksii = new Human("Oleksii" , new Date() , Human.Gender.MALE , Human.BloodGroup.SECOND);;
+        Driver oleksiiD = new Driver(oleksii ,new DriverLicense(new Date(100,12,12),
+                DriverLicense.Category.A , new Date(200,12,13)));
+        GasolineCar car = new GasolineCar("Tesla", "AM5454KB", "10991099" , DriverLicense.Category.A ,
+                10 , 20 , GasolineCar.Transmission.AUTOMATIC);
+        Route route = new Route(car , oleksiiD , "Kyiv" , "Rivne");
+        System.out.println(route);
+    }
+
+
+    @Test
+    public void creatRouteWithDiferentCategory() throws Exception {
+        Human oleksii = new Human("Oleksii" , new Date() , Human.Gender.MALE , Human.BloodGroup.SECOND);;
+        Driver oleksiiD = new Driver(oleksii ,new DriverLicense(new Date() ,
+                DriverLicense.Category.C , new Date()));
+        GasolineCar car = new GasolineCar("Tesla", "AM5454KB", "10991099" , DriverLicense.Category.A ,
+                10 , 20 , GasolineCar.Transmission.AUTOMATIC);
+        Route route = new Route(car , oleksiiD , "Kyiv" , "Rivne");
+        System.out.println(route);
+    }
+
+
+
+    @Test
+    public void creatRouteWithNull() throws Exception {
+        Human oleksii = null;
+        Driver oleksiiD = new Driver(oleksii ,new DriverLicense(null ,
+                DriverLicense.Category.A , null));
+        GasolineCar car = new GasolineCar(null, null, null , DriverLicense.Category.A ,
+                0 , 0 , null);
+        Route route = new Route(car , oleksiiD , null , null);
+        System.out.println(route);
+    }
+}
